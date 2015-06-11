c19a6a7d191524d5ae8172f4a1445458a714f618
hppavilion1 <nathantronnes@gmail.com>
2015-06-10 17:39:20 -0800

    moved args.py into walscript.py

--- WalScript.py
+++ WalScript.py
@@ -1,4 +1,38 @@
 import lexer, args
+from stack import *
+
+def lexexp(s):
+    return s.split()
+
+def evalarg(exp):
+    s = stack()
+    if exp[1] == 'exp':
+        exp = lexexp(exp)
+        for x in exp:
+            if x == '+':
+                s.add()
+            elif x == '-':
+                s.sub()
+            elif x == '*':
+                s.mult()
+            elif x == '/':
+                s.div()
+            else:
+                try:
+                    s.push(float(x))
+                except:
+                    pass
+            
+    elif exp[1] == 'raw':
+        return exp
+            
+    
+def evalargs(args):
+    r=[]
+    for x in args:
+        r.append(evalarg(x))
+    return r
+
 def run(script, env={}):
     script = lex.lex(script)
 
