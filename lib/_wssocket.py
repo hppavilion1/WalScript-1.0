eb30bfbf6679fa0b059a10f5709d0d00417e1bc4
hppavilion1 <nathantronnes@gmail.com>
2015-06-14 16:16:01 -0800

    Finished basic _wssocket
    Other stuff to be implemented with sock.wal

--- lib/_wssocket.py
+++ lib/_wssocket.py
@@ -1,14 +1,34 @@
-def createsocket(env, *args):
+def createsock(env, *args):
     import socket
     return (socket(socket.AF_INET, socket.SOCK_STREAM), env)
 
-def bindsocket(env, *args):
+def bindsock(env, *args):
+    import socket
     env[args[0]].bind((args[1], args[2]))
     return(None, env)
 
+def closesock(env, *args):
+    import socket
+    env[args[0]].close()
+    return (None, env)
+
 def socklisten(env, *args):
+    import socket
     env[args[0]].listen(args[1])
     return (None, env)
 
 def sockrecv(env, *args):
-    return args[0].recv(args[1])
+    import socket
+    return (args[0].recv(args[1]), env)
+
+def socksend(env, *args):
+    import socket
+    env[args[0]].sendall(''.join(args))
+    return (None, env)
+
+self._createsock = createsock
+self._bindsock = bindsock
+self._closesock = closesock
+self._socklisten = socklisten
+self._sockrecv = sockrecv
+self._socksend = socksend
